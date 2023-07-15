cb5caa35af80419d3044d1bbc450117d380cd3c5
ahadsenja <hamdanilm96@gmail.com>
2023-07-16 02:46:29 +0800

    chore: update page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import FilterableProductTable from './components/FilterableProductTable'
 
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <main className="flex flex-col w-1/3 p-5 bg-[#0F172A] rounded">
       <FilterableProductTable />
     </main>
   )
