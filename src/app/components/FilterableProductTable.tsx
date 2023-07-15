928e8588791718393df633607526825bd5ba12b5
ahadsenja <hamdanilm96@gmail.com>
2023-07-16 02:47:05 +0800

    chore: change file name and import

--- src/app/components/FilterableProductTable.tsx
+++ src/app/components/FilterableProductTable.tsx
@@ -1,10 +1,13 @@
 'use client'
 import React from 'react'
+import SearchBar from './SearchBar/SearchBar'
+import ProductTable from './Product/ProductTable'
 
 export default function FilterableProductTable() {
   return (
     <>
-      
+      <SearchBar />
+      <ProductTable />
     </>
   )
 }
