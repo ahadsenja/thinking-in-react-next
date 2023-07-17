f316361aeb23ca864d52cc005016786ba26bb757
ahadsenja <hamdanilm96@gmail.com>
2023-07-17 20:10:01 +0800

    chore: add filterText and inStockOnly state and send it as props to their components

--- src/app/components/FilterableProductTable.tsx
+++ src/app/components/FilterableProductTable.tsx
@@ -1,13 +1,16 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar/SearchBar'
 import ProductTable from './Product/ProductTable'
 
 export default function FilterableProductTable({ products }: { products: any[] }) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
   return (
     <>
-      <SearchBar />
-      <ProductTable products={products} />
+      <SearchBar filterText={filterText} inStockOnly={inStockOnly} onFilterTextOnChange={setFilterText} onInStockOnlyOnChange={setInStockOnly} />
+      <ProductTable products={products} filterText={filterText} inStockOnly={inStockOnly} />
     </>
   )
 }
