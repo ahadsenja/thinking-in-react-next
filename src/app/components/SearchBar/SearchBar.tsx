ec712b61eddbf803c3915f3a844402c862618d29
ahadsenja <hamdanilm96@gmail.com>
2023-07-17 20:12:52 +0800

    chore: add filterText, inStockOnly onFilterTeextChange and onInStockOnlyChange props and use it as event to give an input to the search input to filtering the product data

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
-export default function SearchBar() {
+interface SearchBarIface { 
+  filterText: string, 
+  inStockOnly: boolean,
+  onFilterTextOnChange: (e: any) => void,
+  onInStockOnlyOnChange: (e: any) => void
+}
+
+export default function SearchBar({ filterText, inStockOnly, onFilterTextOnChange, onInStockOnlyOnChange }: SearchBarIface) {
   return (
     <form className="flex flex-col gap-2 w-full mb-5">
-      <input className="px-2 py-2 rounded" type="search" placeholder="Search products..." />
+      <input 
+        className="px-2 py-2 rounded text-gray-900" 
+        type="text" 
+        value={filterText}
+        onChange={(e) => onFilterTextOnChange(e.target.value)}
+        placeholder="Search products..." 
+      />
       <label htmlFor="search">
-        <input type="checkbox" />
+        <input 
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyOnChange(e.target.checked)}
+        />
         {" "}
         Only show products in stock
       </label>
